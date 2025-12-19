405effe142cf843551e617ba8153d9380973ad92
v-srinath.sirimalla <srinathsirimalla18@gmail.com>
2025-12-19 16:29:54 +0530

    APACHE POI 12192025

--- src/test/java/ApachePOI/DataReadExcel.java
+++ src/test/java/ApachePOI/DataReadExcel.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class DataReadExcel {
     @Test
@@ -21,14 +22,23 @@ public class DataReadExcel {
         Sheet sheet = workbook.getSheet("Sheet1");
         sheet.getPhysicalNumberOfRows();
 
-        for(int rowIndex=sheet.getFirstRowNum()+1;rowIndex<=sheet.getLastRowNum();rowIndex++){
-            for(int colIndex=sheet.getRow(1).getFirstCellNum();colIndex<sheet.getRow(1).getLastCellNum();colIndex++){
-                System.out.println("Each Cell value is : "+sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue());
+        String[][] data = new String[sheet.getPhysicalNumberOfRows() - 1][sheet.getRow(1).getPhysicalNumberOfCells()];
+
+        for (int rowIndex = sheet.getFirstRowNum() + 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+            for (int colIndex = sheet.getRow(1).getFirstCellNum(); colIndex < sheet.getRow(1).getLastCellNum(); colIndex++) {
+                //System.out.println("Each Cell value is : "+sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue());
+                data[rowIndex - 1][colIndex] = sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue();
             }
+        }
+        //System.out.println("Data from excel is : "+ Arrays.deepToString(data));
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " | ");
+            }
+            System.out.println("");
         }
         workbook.close();
         fis.close();
-
-
     }
 }

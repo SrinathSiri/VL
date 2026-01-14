4739798bb26cd411bc88167c0d1c6579ae9ff8dd
v-srinath.sirimalla <srinathsirimalla18@gmail.com>
2026-01-14 19:30:26 +0530

    Groups 14012026

--- src/test/java/TestNGPractice/GroupEx1.java
+++ src/test/java/TestNGPractice/GroupEx1.java
@@ -4,17 +4,17 @@ import org.testng.annotations.Test;
 
 public class GroupEx1 {
     @Test(groups = {"smoke"})
-    public void smoke(){
+    public void smoke() {
         System.out.println("Smoke Testing");
     }
 
     @Test(groups = {"regression"})
-    public void regression(){
+    public void regression() {
         System.out.println("Regression Testing");
     }
 
     @Test(groups = {"sanity"})
-    public void sanity(){
+    public void sanity() {
         System.out.println("Sanity Testing");
     }
 }
